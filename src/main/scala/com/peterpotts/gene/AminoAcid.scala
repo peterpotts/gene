9e8dad15b2b849c2dd947607a09bdfeef0cb0f88
Peter Potts <peter.potts@cointrust.com>
2016-09-18 19:19:00 -0700

    Add Codon stuff.

--- src/main/scala/com/peterpotts/gene/AminoAcid.scala
+++ src/main/scala/com/peterpotts/gene/AminoAcid.scala
@@ -2,76 +2,57 @@ package com.peterpotts.gene
 
 import scala.collection.immutable.IndexedSeq
 
-sealed abstract class AminoAcid(val code: Char) extends Enumerated
+sealed abstract class AminoAcid(val abbreviation: String, val code: Char, codons: Set[String]) extends Enumerated
 
-case object Alanine extends AminoAcid('A')
+case object Alanine extends AminoAcid("Ala", 'A', Set("GCT", "GCC", "GCA", "GCG"))
+case object Arginine extends AminoAcid("Arg", 'R', Set("CGT", "CGC", "CGA", "CGG", "AGA", "AGG"))
+case object Asparagine extends AminoAcid("Asn", 'N', Set("AAT", "AAC"))
+case object AsparticAcid extends AminoAcid("Asp", 'D', Set("GAT", "GAC"))
+case object Cysteine extends AminoAcid("Cys", 'C', Set("TGT", "TGC"))
+case object Glutamine extends AminoAcid("Gln", 'Q', Set("CAA, CAG"))
+case object GlutamicAcid extends AminoAcid("Glu", 'E', Set("GAA", "GAG"))
+case object Glycine extends AminoAcid("Gly", 'G', Set("GGT", "GGC", "GGA", "GGG"))
+case object Histidine extends AminoAcid("His", 'H', Set("CAT", "CAC"))
+case object Isoleucine extends AminoAcid("Ile", 'I', Set("ATT", "ATC", "ATA"))
+case object Leucine extends AminoAcid("Leu", 'L', Set("TTA", "TTG", "CTT", "CTC", "CTA", "CTG"))
+case object Lysine extends AminoAcid("Lys", 'K', Set("AAA", "AAG"))
+case object Methionine extends AminoAcid("Met", 'M', Set("ATG"))
+case object Phenylalanine extends AminoAcid("Phe", 'F', Set("TTT", "TTC"))
+case object Proline extends AminoAcid("Pro", 'P', Set("CCT", "CCC", "CCA", "CCG"))
+case object Serine extends AminoAcid("Ser", 'S', Set("TCT", "TCC", "TCA", "TCG", "AGT, AGC"))
+case object Threonine extends AminoAcid("Thr", 'T', Set("ACT", "ACC", "ACA", "ACG"))
+case object Tryptophan extends AminoAcid("Trp", 'W', Set("TGG"))
+case object Tyrosine extends AminoAcid("Tyr", 'Y', Set("TAT", "TAC"))
+case object Valine extends AminoAcid("Val", 'V', Set("GTT", "GTC", "GTA", "GTG"))
 
-case object Cysteine extends AminoAcid('C')
-
-case object AsparticAcid extends AminoAcid('D')
-
-case object GlutamicAcid extends AminoAcid('E')
-
-case object Phenylalanine extends AminoAcid('F')
-
-case object Glycine extends AminoAcid('G')
-
-case object Histidine extends AminoAcid('H')
-
-case object Isoleucine extends AminoAcid('I')
-
-case object Lysine extends AminoAcid('K')
-
-case object Leucine extends AminoAcid('L')
-
-case object Methionine extends AminoAcid('M')
-
-case object Asparagine extends AminoAcid('A')
-
-case object Pyrrolysine extends AminoAcid('O')
-
-case object Proline extends AminoAcid('P')
-
-case object Glutamine extends AminoAcid('Q')
-
-case object Arginine extends AminoAcid('A')
-
-case object Serine extends AminoAcid('S')
-
-case object Threonine extends AminoAcid('T')
-
-case object Selenocysteine extends AminoAcid('U')
-
-case object Valine extends AminoAcid('V')
-
-case object Tryptophan extends AminoAcid('W')
-
-case object Tyrosine extends AminoAcid('Y')
+case object Selenocysteine extends AminoAcid("Sec", 'U', Set("UGA"))
+case object Pyrrolysine extends AminoAcid("Pyl", 'O', Set("UAG"))
 
 object AminoAcid extends Enumeration[AminoAcid] {
   val indexedSeq = IndexedSeq(
-    Alanine,
-    Cysteine,
-    AsparticAcid,
-    GlutamicAcid,
-    Phenylalanine,
-    Glycine,
-    Histidine,
-    Isoleucine,
-    Lysine,
-    Leucine,
-    Methionine,
-    Asparagine,
-    Pyrrolysine,
-    Proline,
-    Glutamine,
-    Arginine,
-    Serine,
-    Threonine,
-    Selenocysteine,
-    Valine,
-    Tryptophan,
-    Tyrosine)
+  Alanine,
+  Arginine,
+  Asparagine,
+  AsparticAcid,
+  Cysteine,
+  Glutamine,
+  GlutamicAcid,
+  Glycine,
+  Histidine,
+  Isoleucine,
+  Leucine,
+  Lysine,
+  Methionine,
+  Phenylalanine,
+  Proline,
+  Serine,
+  Threonine,
+  Tryptophan,
+  Tyrosine,
+  Valine,
+
+  Selenocysteine,
+  Pyrrolysine)
 
   private val map = indexedSeq.map(aminoAcid => aminoAcid.code -> aminoAcid).toMap
 
--- src/main/scala/com/peterpotts/gene/Codon.scala
+++ src/main/scala/com/peterpotts/gene/Codon.scala
@@ -10,7 +10,7 @@ case class Codon(
 }
 
 object Codon {
-  def apply(sequence: String) = {
+  def apply(sequence: String): Codon = {
     Codon(
       first = Nucleotide(sequence.charAt(0)),
       second = Nucleotide(sequence.charAt(1)),
@@ -19,26 +19,6 @@ object Codon {
 }
 
 /**
-  * Alanine Ala/A	GCT, GCC, GCA, GCG
-  * Arginine Arg/R	CGT, CGC, CGA, CGG, AGA, AGG
-  * Asparagine Asn/N	AAT, AAC
-  * Aspartic acid Asp/D	GAT, GAC
-  * Cysteine Cys/C	TGT, TGC
-  * Glutamine Gln/Q	CAA, CAG
-  * Glutamic acid Glu/E	GAA, GAG
-  * Glycine Gly/G	GGT, GGC, GGA, GGG
-  * Histidine His/H	CAT, CAC
-  * Isoleucine Ile/I	ATT, ATC, ATA
   * START	ATG
-  * Leucine Leu/L	TTA, TTG, CTT, CTC, CTA, CTG
-  * Lysine Lys/K	AAA, AAG
-  * Methionine Met/M	ATG
-  * Phenylalanine Phe/F	TTT, TTC
-  * Proline Pro/P	CCT, CCC, CCA, CCG
-  * Serine Ser/S	TCT, TCC, TCA, TCG, AGT, AGC
-  * Threonine Thr/T	ACT, ACC, ACA, ACG
-  * Tryptophan Trp/W	TGG
-  * Tyrosine Tyr/Y	TAT, TAC
-  * Valine Val/V	GTT, GTC, GTA, GTG
   * STOP	TAA, TGA, TAG (Ochre, Opal, Amber)
   */
