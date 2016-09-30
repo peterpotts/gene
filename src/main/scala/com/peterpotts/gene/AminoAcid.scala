587f70e316567f347c6b9a9f6f220f925a905472
Peter Potts <peter.potts@cointrust.com>
2016-09-29 19:55:41 -0700

    Add Protein chains.

--- src/main/scala/com/peterpotts/gene/AminoAcid.scala
+++ src/main/scala/com/peterpotts/gene/AminoAcid.scala
@@ -8,17 +8,17 @@ case object Alanine extends AminoAcid("Ala", 'A') with AminoAcid.Nonpolar
 
 case object Arginine extends AminoAcid("Arg", 'R') with AminoAcid.Basic
 
-case object Asparagine extends AminoAcid("Asn", 'N') with AminoAcid.AmidePolar
+case object Asparagine extends AminoAcid("Asn", 'N') with AminoAcid.Amide
 
 case object AsparticAcid extends AminoAcid("Asp", 'D') with AminoAcid.Acidic
 
-case object Cysteine extends AminoAcid("Cys", 'C') with AminoAcid.DisulfideNonpolar
+case object Cysteine extends AminoAcid("Cys", 'C') with AminoAcid.Disulfide
 
-case object Glutamine extends AminoAcid("Gln", 'Q') with AminoAcid.AmidePolar
+case object Glutamine extends AminoAcid("Gln", 'Q') with AminoAcid.Amide
 
 case object GlutamicAcid extends AminoAcid("Glu", 'E') with AminoAcid.Acidic
 
-case object Glycine extends AminoAcid("Gly", 'G') with AminoAcid.HydrogenNonpolar
+case object Glycine extends AminoAcid("Gly", 'G') with AminoAcid.Hydrogen
 
 case object Histidine extends AminoAcid("His", 'H') with AminoAcid.Basic
 
@@ -32,15 +32,15 @@ case object Methionine extends AminoAcid("Met", 'M') with AminoAcid.Nonpolar
 
 case object Phenylalanine extends AminoAcid("Phe", 'F') with AminoAcid.Nonpolar
 
-case object Proline extends AminoAcid("Pro", 'P') with AminoAcid.NitrogenNonpolar
+case object Proline extends AminoAcid("Pro", 'P') with AminoAcid.Nitrogen
 
-case object Serine extends AminoAcid("Ser", 'S') with AminoAcid.HydroxyPolar
+case object Serine extends AminoAcid("Ser", 'S') with AminoAcid.Hydroxy
 
-case object Threonine extends AminoAcid("Thr", 'T') with AminoAcid.HydroxyPolar
+case object Threonine extends AminoAcid("Thr", 'T') with AminoAcid.Hydroxy
 
 case object Tryptophan extends AminoAcid("Trp", 'W') with AminoAcid.Nonpolar
 
-case object Tyrosine extends AminoAcid("Tyr", 'Y') with AminoAcid.HydroxyPolar
+case object Tyrosine extends AminoAcid("Tyr", 'Y') with AminoAcid.Hydroxy
 
 case object Valine extends AminoAcid("Val", 'V') with AminoAcid.Nonpolar
 
@@ -97,26 +97,30 @@ object AminoAcid {
 
   trait Charged extends Hydrophilic
 
-  trait PositivelyCharged extends Charged
+  trait Positive extends Charged
 
-  trait NegativelyCharged extends Charged
+  trait Negative extends Charged
 
-  trait Nonpolar extends Hydrophobic
+  trait Uncharged
 
-  trait HydrogenNonpolar extends Nonpolar
+  trait Neutral extends Uncharged
 
-  trait NitrogenNonpolar extends Nonpolar
+  trait Nonpolar extends Neutral with Hydrophobic
 
-  trait DisulfideNonpolar extends Nonpolar
+  trait Hydrogen extends Nonpolar
 
-  trait Polar extends Hydrophilic
+  trait Nitrogen extends Nonpolar
 
-  trait AmidePolar extends Polar
+  trait Disulfide extends Nonpolar
 
-  trait HydroxyPolar extends Polar
+  trait Polar extends Neutral with Hydrophilic
 
-  trait Acidic extends NegativelyCharged
+  trait Amide extends Polar
 
-  trait Basic extends PositivelyCharged
+  trait Hydroxy extends Polar
+
+  trait Acidic extends Negative
+
+  trait Basic extends Positive
 
 }
