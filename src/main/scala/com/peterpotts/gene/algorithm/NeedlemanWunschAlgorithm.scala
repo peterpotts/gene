d50d4a8a9e1cbfa1b860c6915cfac2657008005b
Peter Potts <peter.potts@cointrust.com>
2016-10-03 16:32:00 -0700

    NeedlemanWunschAlgorithm complete.

--- src/main/scala/com/peterpotts/gene/algorithm/NeedlemanWunschAlgorithm.scala
+++ src/main/scala/com/peterpotts/gene/algorithm/NeedlemanWunschAlgorithm.scala
@@ -1,87 +1,61 @@
 package com.peterpotts.gene.algorithm
 
-object NeedlemanWunschAlgorithm {
-  val mapMap = Map(
-      'A' -> Map('A' -> 10, 'G' -> -1, 'C' -> -3, 'T' -> -4),
-      'G' -> Map('A' -> -1, 'G' -> 7, 'C' -> -5, 'T' -> -3),
-      'C' -> Map('A' -> -3, 'G' -> -5, 'C' -> 9, 'T' -> 0),
-      'T' -> Map('A' -> -4, 'G' -> -3, 'C' -> 0, 'T' -> 8))
+import scala.collection.mutable
 
-  def S(a: Char)(b: Char) = if (a == b) 1 else -1
-//  def S(a: Char)(b: Char) =mapMap(a)(b)
+case class NeedlemanWunschAlgorithm(similarity: Char => Char => Int, gapPenalty: Int) {
+  private val s = similarity
+  private val d = gapPenalty
 
-  val similarity = S _
-
-//  val A = "GCATGCU".toCharArray
-//  val B = "GATTACA".toCharArray
-//  val A = "AGACTAGTTAC".toCharArray
-//  val B = "CGAGACGT".toCharArray
-
-
-
-  val gapPenalty = -1
-//  val gapPenalty = -5
-
-  val d = gapPenalty
-
-  //align("GCATGCU"->"GATTACA")
-
-  def align(sequences: (String, String)): (String,String) = {
-    val A = sequences._1.toCharArray
-    val B = sequences._2.toCharArray
-
-    val F: Array[Array[Int]] = computeMatrix(A, B)
-
-    computeAlignment(A,B,F)
+  def align(sequences: (String, String)): (String, String) = {
+    val a: IndexedSeq[Char] = sequences._1
+    val b: IndexedSeq[Char] = sequences._2
+    val table = computeTable(a, b)
+    computeAlignment(a, b, table)
   }
 
-  def computeMatrix(A: Array[Char], B: Array[Char]): Array[Array[Int]] = {
-    val F = Array.fill(A.length + 1)(Array.fill(B.length + 1)(0))
+  private def computeTable(a: IndexedSeq[Char], b: IndexedSeq[Char]): Array[Array[Int]] = {
+    val table = Array.fill(a.length + 1)(Array.fill(b.length + 1)(0))
 
-    for (i <- 0 to A.length)
-      F(i)(0) = d * i
+    for (i <- 0 to a.length)
+      table(i)(0) = d * i
 
-    for (j <- 0 to B.length)
-      F(0)(j) = d * j
+    for (j <- 0 to b.length)
+      table(0)(j) = d * j
 
-    for (i <- 1 to A.length)
-      for (j <- 1 to B.length) {
-        val Match = F(i - 1)(j - 1) + S(A(i - 1))(B(j - 1))
-        val Delete = F(i - 1)(j) + d
-        val Insert = F(i)(j - 1) + d
-        F(i)(j) = math.max(Match, math.max(Insert, Delete))
+    for (i <- 1 to a.length)
+      for (j <- 1 to b.length) {
+        val similar = table(i - 1)(j - 1) + s(a(i - 1))(b(j - 1))
+        val delete = table(i - 1)(j) + d
+        val insert = table(i)(j - 1) + d
+        table(i)(j) = math.max(similar, math.max(insert, delete))
       }
 
-    for (i <- 0 to A.length) {
-      println(F(i).mkString("\t"))
-    }
-    F
+    table
   }
 
-  def computeAlignment(A:Array[Char],B:Array[Char],F: Array[Array[Int]]): (String, String) = {
-    var AlignmentA = ""
-    var AlignmentB = ""
-    var i = A.length
-    var j = B.length
+  private def computeAlignment(a: IndexedSeq[Char], b: IndexedSeq[Char], table: Array[Array[Int]]): (String, String) = {
+    val alignmentA = mutable.StringBuilder.newBuilder
+    val alignmentB = mutable.StringBuilder.newBuilder
+    var i = a.length
+    var j = b.length
 
     while (i > 0 && j > 0) {
-      if (i > 0 && j > 0 && F(i)(j) == F(i - 1)(j - 1) + S(A(i - 1))(B(j - 1))) {
-        AlignmentA = A(i - 1) + AlignmentA
-        AlignmentB = B(j - 1) + AlignmentB
-        i = i - 1
-        j = j - 1
-      } else if (i > 0 && F(i)(j) == F(i - 1)(j) + d) {
-        AlignmentA = A(i - 1) + AlignmentA
-        AlignmentB = "-" + AlignmentB
-        i = i - 1
+      if (i > 0 && j > 0 && table(i)(j) == table(i - 1)(j - 1) + s(a(i - 1))(b(j - 1))) {
+        alignmentA += a(i - 1)
+        alignmentB += b(j - 1)
+        i -= 1
+        j -= 1
+      } else if (i > 0 && table(i)(j) == table(i - 1)(j) + d) {
+        alignmentA += a(i - 1)
+        alignmentB += '-'
+        i -= 1
       } else {
-        AlignmentA = "-" + AlignmentA
-        AlignmentB = B(j - 1) + AlignmentB
-        j = j - 1
+        alignmentA += '-'
+        alignmentB += b(j - 1)
+        j -= 1
       }
     }
-    (AlignmentA, AlignmentB)
+
+    alignmentA.reverse.toString -> alignmentB.reverse.toString
   }
-
-
 }
--- src/test/scala/com/peterpotts/gene/algortithm/NeedlemanWunschAlgorithmTest.scala
+++ src/test/scala/com/peterpotts/gene/algortithm/NeedlemanWunschAlgorithmTest.scala
@@ -5,8 +5,11 @@ import org.scalatest.mock.MockitoSugar
 import org.scalatest.{Matchers, WordSpec}
 
 class NeedlemanWunschAlgorithmTest extends WordSpec with Matchers with MockitoSugar {
+  val similarity = (a: Char) => (b: Char) => if (a == b) 1 else -1
+  val gapPenalty = -1
+
   "The Needleman-Wunsch algorithm" should {
-    "align sequences" in {
+    "align nucleotide sequences" in {
       val sequences = "GCATGCU" -> "GATTACA"
 
       val alignments = Set(
@@ -14,9 +17,14 @@ class NeedlemanWunschAlgorithmTest extends WordSpec with Matchers with MockitoSu
         "GCA-TGCU" -> "G-ATTACA",
         "GCAT-GCU" -> "G-ATTACA")
 
-      val alignment = NeedlemanWunschAlgorithm.align(sequences)
+      val alignment = NeedlemanWunschAlgorithm(similarity, gapPenalty).align(sequences)
+      (alignments contains alignment) shouldBe true
+    }
 
-      println(alignment)
+    "align string sequences" in {
+      val sequences = "THISISASEQUENCE" -> "THATSEQUENCE"
+      val alignments = Set("THISISA-SEQUENCE" -> "TH----ATSEQUENCE")
+      val alignment = NeedlemanWunschAlgorithm(similarity, gapPenalty).align(sequences)
       (alignments contains alignment) shouldBe true
     }
   }
